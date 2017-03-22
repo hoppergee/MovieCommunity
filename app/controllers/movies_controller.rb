b3f42f5c3079862638e973787553bdadc7b0be80
Ji Xiang <jx4554@gmail.com>
2017-03-22 23:23:26 +0800

    implement movies#destroy

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -30,6 +30,13 @@ class MoviesController < ApplicationController
     redirect_to movies_path, notice: "Update Scuccess"
   end
   
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:alert] = "Movie deleted"
+    redirect_to movies_path
+  end
+  
   private
   def movie_params
     params.require(:movie).permit(:title, :theaterdate, :description)
