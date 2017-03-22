341d62b15cc98149b4cd154ea7d3bd32e4b4ec35
Ji Xiang <jx4554@gmail.com>
2017-03-22 23:19:45 +0800

    implement movies#update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -22,6 +22,14 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
   
+  def update
+    @movie = Movie.find(params[:id])
+    
+    @movie.update(movie_params)
+    
+    redirect_to movies_path, notice: "Update Scuccess"
+  end
+  
   private
   def movie_params
     params.require(:movie).permit(:title, :theaterdate, :description)
